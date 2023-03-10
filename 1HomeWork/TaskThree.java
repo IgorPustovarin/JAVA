ceae7960a8b8659fa38dffc4789489565211d54f
Igor <adel-time@mail.ru>
2023-03-10 19:17:57 +0800

    Первая ДЗ JAVA

---
+++ 1HomeWork/TaskThree.java
@@ -0,0 +1,25 @@
+// Реализовать простой калькулятор (+ - / *)
+// Ввод числа ->
+// Ввод знака ->
+// Ввод числа ->
+package HomeWork1;
+
+import java.util.Scanner;
+
+public class TaskThree {
+    public static void main(String[] args) {
+        Scanner number1 = new Scanner(System.in);
+        System.out.println("Введи первое число ");
+        int num1 = number1.nextInt();
+        number1.close();
+    
+    }
+    
+}
+
+
+
+// Scanner name = new Scanner(System.in);
+// //         System.out.print("Name is ");
+// //         String name1 = name.nextLine();
+// //         name.close();
---
+++ 1HomeWork/TaskTwo.java
@@ -0,0 +1,21 @@
+// Вывести все простые числа от 1 до 1000
+
+package HomeWork1;
+
+public class TaskTwo {
+    public static void main(String[] args) {
+        int number = 1000;
+        int count = 1;
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) count++;
+                
+            }
+            if (count == 2){
+                System.out.print(" " + i + " ");
+            }            
+            count = 0;
+        }
+    }
+    
+}
