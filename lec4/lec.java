c75813aebf7330615d0cabe79316d3ba39d83d12
Igor <adel-time@mail.ru>
2023-03-23 18:05:45 +0800

    запушиваю работы по JAVA

---
+++ lec4/lec.java
@@ -0,0 +1,40 @@
+package lec4;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+// public class lec {
+//     public static void main(String[] args) {
+//         Queue<Integer> queue = new LinkedList<Integer>();
+//         queue.add(1);
+//         queue.add(2);
+//         int item = queue.remove();//Удаление элемента, удаляется элемент, который бул добавлен в первую очередь.
+//         LinkedList<Integer> ll = new LinkedList<Integer>();
+
+//         ll.add(1);
+//         ll.add(2);
+//         ll.add(3);
+//     }
+    
+// }
+/**
+ * lec
+ */
+// public class lec {
+//     public static void main(String[] args) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+//         pq.add(123);
+//         pq.add(3);
+//         pq.add(13);
+//         pq.add(1);
+//         System.out.println(pq);
+//         System.out.println(pq.poll());
+//         System.out.println(pq.poll());
+//         System.out.println(pq.poll());
+//         System.out.println(pq.poll());
+//         System.out.println(pq.poll());
+//     }
+
+    
+// }
