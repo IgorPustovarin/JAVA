939c62bed4e93fbc7f9ff63d92c0d6eb77d78ba4
Igor <adel-time@mail.ru>
2023-03-25 13:01:00 +0800

    Сделал третью  задачу 5го домашнего

--- h55HomeWork/Task3.java
+++ h55HomeWork/Task3.java
@@ -1,8 +1,42 @@
+
 package h55HomeWork;
 
 public class Task3 {
     public static void main(String[] args) {
+        int[] ferz = new int[8];
+        stepMotion(0, ferz);  
+        for (int horizontal = 0; horizontal < 8; horizontal++) {
+            for (int vertical = 0; vertical < 8; vertical++) {
+                if (ferz[horizontal] == vertical) {
+                    System.out.print(" X ");
+                } 
+                else System.out.print(" _ ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean stepMotion(int count, int[] queenArray) {
+        if (count == 8) {
+            return true;
+        }
+        for (int vertical = 0; vertical < 8; vertical++) {
+            boolean positionQ = true;
+            for (int i = 0; i < count; i++) {               
+                if (queenArray[i] == vertical || queenArray[i] == vertical - count + i || queenArray[i] == vertical + count - i) { 
+                    positionQ = false;
+                    break;
+                }
+            }
+            if (positionQ) {
+                queenArray[count] = vertical;
+                if (stepMotion(count + 1, queenArray)) {
+                return true;
+                }
+            }
+        }
+        return false;
        
     }
 }
-}
+
